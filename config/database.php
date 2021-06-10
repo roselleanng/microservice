1d105b01d0b798671fcc38b76d23855b20bec99f
unknown <roselleannguzman14@gmail.com>
2021-06-10 13:46:33 +0800

    Added Config Database.php in App.php

--- config/database.php
+++ config/database.php
@@ -48,19 +48,14 @@ before you begin development.
  'mysql' => [ 'driver' => 'mysql',
  'host' => env('DB_HOST', null),
  'port' => env('DB_PORT', null),
- 'database' => env('DB_DATABASE', 
-null),
- 'username' => 
-env('DB_USERNAME', null),
- 'password' => 
-env('DB_PASSWORD', null),
- 'unix_socket' => env('DB_SOCKET', 
-''),
- 'charset' => env('DB_CHARSET', 
-'utf8'),
- 'collation' => env('DB_COLLATION', 
-'utf8_unicode_ci'),
+ 'database' => env('DB_DATABASE',null),
+ 'username' => env('DB_USERNAME', null),
+ 'password' => env('DB_PASSWORD', null),
+ 'unix_socket' => env('DB_SOCKET', ''),
+ 'charset' => env('DB_CHARSET', 'utf8'),
+ 'collation' => env('DB_COLLATION','utf8_unicode_ci'),
  'prefix' => env('DB_PREFIX', ''),
+
  // 'strict' => 
 env('DB_STRICT_MODE', false),
  // 'engine' => env('DB_ENGINE', 
